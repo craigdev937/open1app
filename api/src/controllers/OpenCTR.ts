aaa103dd942914a6f2f1e19382aae9346734a993
Craig Johnson <craigdev937@gmail.com>
2026-06-08 21:47:49 -0400

    Updated OpenCTR and created ChatService.

--- api/src/controllers/OpenCTR.ts
+++ api/src/controllers/OpenCTR.ts
@@ -1,7 +1,6 @@
 import express from "express";
-import { client } from "../middleware/openai.ts";
 import { CSchema } from "../validation/Schema.ts";
-import { conRepo } from "../data/ConREP.ts";
+import { CService } from "../service/ChatService.ts";
 
 class OpenAI {
     Chat: express.Handler = async (req, res, next) => {
@@ -12,18 +11,10 @@ class OpenAI {
                 return;
             };
             const { prompt, convId } = req.body;
-            const response = await client.responses.create({
-                model: "gpt-5.4-nano-2026-03-17",
-                input: prompt,
-                temperature: 0.2,
-                max_output_tokens: 100,
-                previous_response_id: conRepo.getLastResID(convId)
-            });
-            conRepo.setLastResID(convId, response.id);
-            
+            const response = await CService.sendMessage(prompt, convId);            
             return res
                 .status(201)
-                .json({ message: response.output_text });
+                .json({ message: response.message });
         } catch (error) {
             res
                 .status(500)
---
+++ api/src/service/ChatService.ts
@@ -0,0 +1,27 @@
+import { client } from "../middleware/openai.ts";
+import { conRepo } from "../data/ConREP.ts";
+
+interface ChatRes {
+    id: string,
+    message: string
+};
+
+// Public Interface and Leaky Abstraction.
+export const CService = {
+    async sendMessage(prompt: string, convId: string): Promise<ChatRes> {
+        const response = await client.responses.create({
+            model: "gpt-5.4-nano-2026-03-17",
+            input: prompt,
+            temperature: 0.2,
+            max_output_tokens: 100,
+            previous_response_id: conRepo.getLastResID(convId)
+        });
+        conRepo.setLastResID(convId, response.id);
+        return {
+            id: response.id,
+            message: response.output_text
+        };
+    }
+};
+
+
